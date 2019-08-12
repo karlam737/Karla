730c3109a593568ed2e79ebcac6f6d753c37c875
karlam737 <54039923+karlam737@users.noreply.github.com>
2019-08-12 14:33:14 -0400

    Add files via upload

---
+++ K.V.M FILE.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print ("hello world")
+print ("how's life?")
+print("13")
+print("Karla")
+("5*2")
+("5/2")
+("5-2")
+("5**2")
+("8/9*3")
